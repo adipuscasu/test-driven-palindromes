0f3868a111da64b6999c2de123bee889ef10e612
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2024-12-13 23:18:22 +0200

    :wrench: Prettier configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   plugins: ["jest"],
-  extends: ["eslint:recommended", "plugin:jest/recommended"],
+  extends: ["eslint:recommended", "plugin:jest/recommended", "prettier"],
   env: {
-    "node": true,
-    "es2021": true
+    node: true,
+    es2021: true,
   },
   overrides: [
     {
