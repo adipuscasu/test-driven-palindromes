a7d33d2809f06a2497ef679e3d632df178f1cadc
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2024-12-14 00:12:11 +0200

    :white_check_mark: passing tests

--- palindromes.js
+++ palindromes.js
@@ -1,3 +1,18 @@
+const prepareString = (str) => str.toLowerCase().replace(/[^a-z]/g, "");
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+const isPalindrome = (str) => {
+  const sanitizedStr = prepareString(str);
+  return sanitizedStr === reverseString(sanitizedStr);
+};
+
 module.exports = (str) => {
-    return [str];
+  const sanitizedStr = prepareString(str);
+  if (!isPalindrome(sanitizedStr)) {
+    return [];
+  }
+  return [prepareString(str)];
 };
