ea4ad3525d71c43cd0cd85ece230188d337a87fe
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2024-12-16 23:28:12 +0200

    adds unit tests for helper functions

--- palindromes.test.js
+++ palindromes.test.js
@@ -1,4 +1,5 @@
 const palindromes = require("./palindromes");
+const { prepareString, isPalindrome } = palindromes;
 
 describe("palindromes()", () => {
   it("correctly identifies one-word palindromes", () => {
@@ -21,5 +22,29 @@ describe("palindromes()", () => {
     expect(palindromes("A man, a plan, a canal, Panama")).toEqual([
       "amanaplanacanalpanama",
     ]);
+  });
+});
+
+describe("prepareString()", () => {
+  it("converts the given string to lowercase", () => {
+    expect(prepareString("aAaA")).toBe("aaaa");
+  });
+
+  it("removes all non-letter characters", () => {
+    expect(prepareString("To infinity, and beyond!")).toBe(
+      "toinfinityandbeyond",
+    );
+  });
+});
+
+describe("isPalindrome()", () => {
+  it("returns true when given a palindrome", () => {
+    expect(isPalindrome("aba")).toBe(true);
+    expect(isPalindrome("abba")).toBe(true);
+  });
+
+  it("returns false when given a non-palindrome", () => {
+    expect(isPalindrome("abb")).toBe(false);
+    expect(isPalindrome("aaba")).toBe(false);
   });
 });
